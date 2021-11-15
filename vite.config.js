6fe81bf6ecc698efde55fd72e351acd02dce1d46
gibbzie <mattygibbs@protonmail.com>
2021-11-15 18:40:31 +0100

    Preparing

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import mkcert from'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    https: true
-  },
+  base: '/CallApp/'
   plugins: [mkcert()]
 })
